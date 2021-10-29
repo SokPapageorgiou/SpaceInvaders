cbd800257905bbf8021ee1906844f30f1fe06689
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-29 10:19:39 +0200

    GridCreator Refactor

--- Assets/Scripts/Alien/GridCreator.cs
+++ Assets/Scripts/Alien/GridCreator.cs
@@ -7,17 +7,43 @@ namespace Grid
     [RequireComponent(typeof(GridStatsLoader))]
     public class GridCreator : MonoBehaviour
     {
-        private GridStats _gridStats;
+        [SerializeField] private Vector2 _gridSize;
+        
+        private StageConstrains _stageConstrains;
         private Queue<GameObject> _alienArmy;
         private GameObject _alien;
 
         private void Awake()
         {
-            GridStatsLoader temp = GetComponent<GridStatsLoader>();
-            _gridStats = temp.gridStats;
-            _alien = temp.alien;
+            LoadComponents();
+            CreatePool();
+            PlaceObjects();
+        }
 
-            _alienArmy = ObjectPool.Create(_alien, _gridStats.TotalUnities());
+        private void LoadComponents()
+        {
+            GridStatsLoader temp = GetComponent<GridStatsLoader>();
+            _alien = temp.alien;
+        }
+
+        private void CreatePool()
+        {
+            _alienArmy = ObjectPool.Create(_alien, ArmySize());
+        }
+
+        private int ArmySize()
+        {
+            return (int) (_gridSize.x * _gridSize.y);
+        }
+
+        private void PlaceObjects()
+        {
+            float pointA;
+            
+            foreach (GameObject alien in _alienArmy)
+            {
+                
+            }
         }
     }
 }
--- Assets/Scripts/Alien/GridStatsLoader.cs
+++ Assets/Scripts/Alien/GridStatsLoader.cs
@@ -1,13 +1,10 @@
-﻿using System.Collections.Generic;
-using Commons;
+﻿using Commons;
 using UnityEngine;
 
 namespace Grid
 {
     public class GridStatsLoader : StageConstrainsLoader
     {
-        public GridStats gridStats;
-
         [Header("Enemies")] 
         public GameObject alien;
     }
